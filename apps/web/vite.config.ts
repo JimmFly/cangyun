dee7eeb8e16f35ed3248c6730030bb71d2504d7a
Jimmfly <yangjinfei001@gmail.com>
2025-10-09 11:16:02 +0800

    chore: remove useless comment

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -4,6 +4,5 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
   plugins: [react(), tailwindcss()],
 });
